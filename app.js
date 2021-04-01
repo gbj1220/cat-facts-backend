49249398f85766cdb4242b32f90cc73eb19a6b5b
Gregory.Johnson <3531op@gmail.com>
2021-04-01 11:03:46 -0400

    Getting the backends newFriend creator to work

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users/usersRouter");
+const friendsRouter = require("./routes/addFriend/friendsRouter");
 
 require("dotenv").config();
 
@@ -28,6 +30,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,6 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/friends", friendsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
